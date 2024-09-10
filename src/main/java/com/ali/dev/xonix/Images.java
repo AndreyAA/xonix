7652ecf422c40677ee5b84caaead9777e98d8197
AndreyAA <ali21net@yandex.ru>
2024-09-10 23:17:13 +0300

    repackage

--- src/main/java/com/ali/dev/xonix/Images.java
+++ src/main/java/com/ali/dev/xonix/Images.java
@@ -7,9 +7,9 @@ import java.io.File;
 import java.io.IOException;
 
 public class Images {
-    public static final Image bonusLife = loadImage("resources/hart20.png");
-    public static final Image speedUp = loadImage("resources/speedup20.png");
-    public static final Image freeze = loadImage("resources/freeze20.png");
+    public static final Image bonusLife = loadImage("resources/images/hart20.png");
+    public static final Image speedUp = loadImage("resources/images/speedup20.png");
+    public static final Image freeze = loadImage("resources/images/freeze20.png");
 
     private static BufferedImage loadImage(String image) {
         try {
